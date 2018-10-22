0ade90a929944939d5e57c37b0888bb6ca680fc9
jakyle <jjackson360@gmail.com>
2018-10-22 19:00:39 -0400

    James TA Review

--- GC-AH Lab 3/Program.cs
+++ GC-AH Lab 3/Program.cs
@@ -16,12 +16,15 @@ namespace GC_AH_Lab_3
             while (true)
             {
                 Console.Write($"{name} Please enter a number from 1 to 100...PLEASE!! (sob) I BEG YOU!!:");
+                // James - I see you are getting used to validation with TryParse(), good job!
                 if (int.TryParse(Console.ReadLine(), out int number))
                 {
+                    // James - just so you know, negative numbers are not handled in this code.
                     EvenAndOddNumbers(name, number);
 
                     while (true)
                     {
+                        // James - This is a neat way to see if you want to stop the program or not
                         Console.WriteLine("Enter 1 to continue or 2 to stop");
                         var answer = Console.ReadLine();
 
@@ -31,16 +34,18 @@ namespace GC_AH_Lab_3
                         }
                         else if (answer == "2")
                         {
+                            // James - I like that you used a return here, just as long as you know what this is doing.
                             return;
                         }
                         else
                         {
+                            // James - lol... nice
                             Console.WriteLine("AGAIN!! 1 to continue and 2 to exit!! Pay attention!! THANK YOU!!");
                         }
                     }
                 }
                 else
-                {
+                {   // James - lol
                     Console.WriteLine("We only take numbers punk!(press any key to start again)");
                 }
                 //Console.ReadKey();
@@ -48,6 +53,7 @@ namespace GC_AH_Lab_3
            
         }
 
+        // James - I love that you put the "business logic" inside of this method! this help organizes your code, good job
         private static void EvenAndOddNumbers(string name, int number)
         {
             if (number % 2 == 0 && number < 25)
@@ -68,6 +74,8 @@ namespace GC_AH_Lab_3
             }
             else
             {
+                // James - I see what you are doing here... and I see from a design choice that you want the user
+                // to decide if they want to run the whole program or not.  I like this decision to be honest
                 Console.WriteLine($"That`s waaaay to high {name}!!!, Please only input numbers 1 - 100:");
             }
         }
