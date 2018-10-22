4cd6646eb97f6a6bfd3bf9ee56760f92557f1247
David Edelstein <David Edelstein>
2018-10-21 23:03:54 -0400

    extracted method

--- GC-AH Lab 3/Program.cs
+++ GC-AH Lab 3/Program.cs
@@ -18,27 +18,7 @@ namespace GC_AH_Lab_3
                 Console.Write($"{name} Please enter a number from 1 to 100...PLEASE!! (sob) I BEG YOU!!:");
                 if (int.TryParse(Console.ReadLine(), out int number))
                 {
-                    
-                    if (number % 2 == 0 && number < 25)
-                    {
-                        Console.WriteLine($"{name} , The number that you have entered - {number} is EVEN and LESS than 25.");
-                    }
-                    else if (number % 2 == 0 && number > 25 && number < 60)
-                    {
-                        Console.WriteLine($"EVEN number {name}!!! HECK YEAH!!!");
-                    }
-                    else if (number % 2 == 0 && number > 60 && number < 101)
-                    {
-                        Console.WriteLine($"Would you believe it {name}? This here {number} is EVEN!!");
-                    }
-                    else if (number % 1 == 0 && number < 100)
-                    {
-                        Console.WriteLine($"{number} is ODD {name}.");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"That`s waaaay to high {name}!!!, Please only input numbers 1 - 100:");
-                    }
+                    EvenAndOddNumbers(name, number);
 
                     while (true)
                     {
@@ -57,19 +37,39 @@ namespace GC_AH_Lab_3
                         {
                             Console.WriteLine("AGAIN!! 1 to continue and 2 to exit!! Pay attention!! THANK YOU!!");
                         }
-                        
                     }
                 }
                 else
                 {
                     Console.WriteLine("We only take numbers punk!(press any key to start again)");
                 }
-               
-
-
-                Console.ReadKey();
+                //Console.ReadKey();
             }
            
+        }
+
+        private static void EvenAndOddNumbers(string name, int number)
+        {
+            if (number % 2 == 0 && number < 25)
+            {
+                Console.WriteLine($"{name} , The number that you have entered - {number} is EVEN and LESS than 25.");
+            }
+            else if (number % 2 == 0 && number > 25 && number < 60)
+            {
+                Console.WriteLine($"EVEN number {name}!!! HECK YEAH!!!");
+            }
+            else if (number % 2 == 0 && number > 60 && number < 101)
+            {
+                Console.WriteLine($"Would you believe it {name}? This here {number} is EVEN!!");
+            }
+            else if (number % 1 == 0 && number < 100)
+            {
+                Console.WriteLine($"{number} is ODD {name}.");
+            }
+            else
+            {
+                Console.WriteLine($"That`s waaaay to high {name}!!!, Please only input numbers 1 - 100:");
+            }
         }
     }
     
